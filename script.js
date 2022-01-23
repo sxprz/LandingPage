cbc378aabc75d55d5a5e949a9df614299ac47b3e
sxprz <derian.pljakic@gmail.com>
2022-01-24 00:56:16 +0100

    Add more javascript and update copyright year in footer

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function about() {
+	alert("© 2022 suprize");
+}
+
+function contact() {
+	alert("Visit\ngithub.com/sxprz\nto look into other projects.");
+}
+
+function references() {
+	alert("Thanks to ProgrammingHub for teaching me how to use basic JavaScript!");
+}
+
 alert("Welcome to suprize's landing page!\nMore JavaScript is coming soon.");
